030080ffdc3d4cd558a850a65f86b43c578ff013
mikusjelly <mikusjelly@gmail.com>
2017-11-30 16:51:10 +0800

    udpate

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', 'r') as f:
 setup(
     name="apkutils",
 
-    version="0.1.1",
+    version="0.1.2",
 
     description=("Utils for parsing apk."),
     long_description=long_description,
