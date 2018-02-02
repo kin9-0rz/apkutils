0c1fb9d43964396a84dbc0158f6dffb5cbc4be18
mikusjelly <mikusjelly@gmail.com>
2018-02-02 15:20:36 +0800

    Fixed #1 Add examples

---
+++ examples/files.py
@@ -0,0 +1,11 @@
+import os
+
+from apkutils.apk import APK
+
+file_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", 'data', 'test'))
+apk = APK(file_path)
+
+files = apk.get_files()
+for item in files:
+    print(item['name'])
---
+++ examples/strings.py
@@ -0,0 +1,14 @@
+import binascii
+import os
+
+from apkutils.apk import APK
+
+file_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "..", 'data', 'test'))
+apk = APK(file_path)
+
+strs = apk.get_strings()  # the strings from all of classes\d*.dex
+for item in strs:
+    s = binascii.unhexlify(item).decode('utf-8', errors='ignore')
+    if 'hello' in s:
+        print(s)
