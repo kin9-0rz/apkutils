0385c39f3ae5b28802661da7999888266b296b34
mikusjelly <mikusjelly@gmail.com>
2020-06-30 15:39:31 +0800

    perf: 优化清单交集

--- apkutils/intersection.py
+++ apkutils/intersection.py
@@ -1,3 +1,4 @@
+import os.path
 import re
 
 
@@ -13,6 +14,36 @@ class APK_Intersection:
 
         action_pattern = r'action\s+?.*?:name="([^"]+?)"'
         self.action_matcher = re.compile(action_pattern)
+
+        category_pattern = r'<category\s[^>]*?:name="([^"]+?)"'
+        self.category_matcher = re.compile(category_pattern)
+
+        uses_feature_pattern = r'<uses-feature\s[^>]*?:name="([^"]+?)"'
+        self.uses_feature_matcher = re.compile(uses_feature_pattern)
+
+        activity_pattern = r'<activity\s[^>]*?:name="([^"]*?)"'
+        self.activity_matcher = re.compile(activity_pattern)
+
+        activity_alias_pattern = r'<activity-alias\s[^>]*?:name="([^"]*?)"'
+        self.activity_alias_matcher = re.compile(activity_alias_pattern)
+
+        receiver_pattern = r'<receiver\s[^>]*?:name="([^"]*?)"'
+        self.receiver_matcher = re.compile(receiver_pattern)
+
+        service_pattern = r'<service\s[^>]*?:name="([^"]*?)"'
+        self.service_matcher = re.compile(service_pattern)
+
+        provider_pattern = r'<provider\s[^>]*?:name="([^"]*?)"'
+        self.provider_matcher = re.compile(provider_pattern)
+
+        meta_name_pattern = r'<meta-data\s[^>]*?:name="([^"]*?)"'
+        self.meta_name_matcher = re.compile(meta_name_pattern)
+
+        meta_value_pattern = r'<meta-data\s[^>]*?:value="([^"]*?)"'
+        self.meta_value_matcher = re.compile(meta_value_pattern)
+
+        label_pattern = r'android:label="([^"]+?)"'
+        self.label_matcher = re.compile(label_pattern)
 
     def get_permissions(self, mani):
         perms = set()
@@ -88,6 +119,23 @@ class APK_Intersection:
 
         return result, nums
 
+    @staticmethod
+    def gen_words(s):
+        words = set()
+        size = len(s)
+
+        for offset in range(1, size+1):
+            for i in range(0, size+1-offset):
+                word = '.'.join(s[i:i+offset])
+                start = ''
+                end = ''
+                if i > 0:
+                    start = '*.'
+                if size - offset > i:
+                    end = '.*'
+                words.add(start + word + end)
+        return words
+
     def intersect_manifest(self):
         """清单交集
 
@@ -104,13 +152,135 @@ class APK_Intersection:
             'version_code': [0xFF, 0],
         }
 
+        import jieba
+
+        is_first = True
+        words = {
+            'uses-permission': set(),
+            'application': set(),
+            'activity': set(),
+            'activity-alias': set(),
+            'receiver': set(),
+            'service': set(),
+            'provider': set(),
+            'action': set(),
+            'meta-data:name': set(),
+            'meta-data:value': set(),
+            'label': set(),
+
+        }
+
         same = None
         for apk in self.apks:
             mani = apk.get_org_manifest()
             if not mani:
                 print(apk.apk_path, 'no mani')
                 continue
+
+            tmp_words = set()
+            application = apk.get_application()
+
+            app_words = APK_Intersection.gen_words(application.split('.'))
+            if is_first:
+                words['application'] = app_words
+            else:
+                words['application'] &= app_words
+
             mani = self.serialize_xml(mani)
+            pieces = set()
+            for item in self.perm1_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['uses-permission'] = pieces
+            else:
+                words['uses-permission'] &= pieces
+
+            pieces = set()
+            for item in self.uses_feature_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['uses-feature'] = pieces
+            else:
+                words['uses-feature'] &= pieces
+
+            pieces = set()
+            for item in self.action_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['action'] = pieces
+            else:
+                words['action'] &= pieces
+
+            pieces = set()
+            for item in self.category_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['category'] = pieces
+            else:
+                words['category'] &= pieces
+
+            pieces = set()
+            for item in self.label_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['label'] = pieces
+            else:
+                words['label'] &= pieces
+
+            pieces = set()
+            for item in self.meta_name_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['meta-data:name'] = pieces
+            else:
+                words['meta-data:name'] &= pieces
+
+            pieces = set()
+            for item in self.meta_value_matcher.finditer(mani):
+                pieces.add(item.groups()[0])
+            if is_first:
+                words['meta-data:value'] = pieces
+            else:
+                words['meta-data:value'] &= pieces
+
+            pieces = set()
+            for item in self.activity_matcher.finditer(mani):
+                piece = APK_Intersection.gen_words(item.groups()[0].split('.'))
+                pieces |= piece
+            if is_first:
+                words['activity'] = pieces
+            else:
+                words['activity'] &= pieces
+
+            pieces = set()
+            for item in self.receiver_matcher.finditer(mani):
+                piece = APK_Intersection.gen_words(item.groups()[0].split('.'))
+                pieces |= piece
+            if is_first:
+                words['receiver'] = pieces
+            else:
+                words['receiver'] &= pieces
+
+            pieces = set()
+            for item in self.service_matcher.finditer(mani):
+                piece = APK_Intersection.gen_words(item.groups()[0].split('.'))
+                pieces |= piece
+            if is_first:
+                words['service'] = pieces
+            else:
+                words['service'] &= pieces
+
+            pieces = set()
+            for item in self.provider_matcher.finditer(mani):
+                piece = APK_Intersection.gen_words(item.groups()[0].split('.'))
+                pieces |= piece
+            if is_first:
+                words['provider'] = pieces
+            else:
+                words['provider'] &= pieces
+
+            if is_first:
+                is_first = False
 
             if not same:
                 same = mani
@@ -128,11 +298,7 @@ class APK_Intersection:
                     mm[1] = value
                 nums[key] = mm
 
-        result = ''
-        if same:
-            result = re.sub(r'"[^"]+?\*[^"]+?"', '"[^"]+?"', same)
-
-        return result, nums
+        return words, nums
 
     def intersect_dex_string_refx(self, filters):
         """字符串交集
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os.path
 
 from setuptools import find_packages, setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
