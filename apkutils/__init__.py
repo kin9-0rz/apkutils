df834542beb09f1fdb1ccd4466b6b5716acd7324
kin9-0rz <kin9-0rz@outlook.com>
2021-06-10 18:04:46 +0800

    Bump version: 0.10.0 → 0.10.1

--- apkutils/__init__.py
+++ apkutils/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """King Orz"""
 __email__ = 'kin9-0rz@outlook.com'
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 
 import binascii
 import re
--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/kin9-0rz/apkutils',
-    version='0.10.0',
+    version='0.10.1',
     zip_safe=False,
 )
